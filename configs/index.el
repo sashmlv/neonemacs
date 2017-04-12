b1e509a12a757142912319fbd4c0db34bc083e58
sashlex <sashlex@yandex.ru>
2017-04-12 21:06:39 +0300

    Fix comments.

--- configs/index.el
+++ configs/index.el
@@ -23,35 +23,35 @@
 (defconst undo_redo_dir (concat base_dir (file-name-as-directory "undo_redo"))) ;; save undo-redo history in this dir
 
 ;; is need use tern for javascript development
-(defconst use_tern_mode t) ;; used in packages-installation.el
+(defconst use_tern_mode t) ;; used in modes-setup/tern-mode-setup.el
 (defconst tern_dir (concat base_dir (file-name-as-directory "tern"))) ;; tern files placed here
 (defconst tern_file (concat tern_dir (file-name-as-directory "emacs") "tern.el")) ;; tern file
 
 (defconst current_font "Menlo-9")
-(defconst current_indent 3) ;; 3 spaces ;; used in indentation.el
-(defconst indentation_color "#101010") ;; used in hooks.el
-(defconst indentation_current_line_color "#101010") ;; used in hooks.el
+(defconst current_indent 3) ;; 3 spaces ;; used in common/indentation.el
+(defconst indentation_color "#101010") ;; used in common/hooks.el
+(defconst indentation_current_line_color "#101010") ;; used in common/hooks.el
 
-(defconst brackets_match_foreground "white") ;; used in base.el
-(defconst brackets_match_background "blue3") ;; used in base.el
-(defconst brackets_mismatch_foreground "white") ;; used in base.el
-(defconst brackets_mismatch_background "red3") ;; used in base.el
+(defconst brackets_match_foreground "white") ;; used in modes-setup/base-modes-setup.el
+(defconst brackets_match_background "blue3") ;; used in modes-setup/base-modes-setup.el
+(defconst brackets_mismatch_foreground "white") ;; used in modes-setup/base-modes-setup.el
+(defconst brackets_mismatch_background "red3") ;; used in modes-setup/base-modes-setup.el
 
-(defconst whitespace_foreground "#212121") ;; used in base.el
-(defconst whitespace_background "#070707") ;; used in base.el
-(defconst whitespace_weight 'light) ;; used in base.el
+(defconst whitespace_foreground "#212121") ;; used in modes-setup/base-modes-setup.el
+(defconst whitespace_background "#070707") ;; used in modes-setup/base-modes-setup.el
+(defconst whitespace_weight 'light) ;; used in modes-setup/base-modes-setup.el
 
-(defconst whitespace_trailing_foreground "#ff2020") ;; used in base.el
-(defconst whitespace_trailing_background "#070707") ;; used in base.el
-(defconst whitespace_trailing_weight 'light) ;; used in base.el
+(defconst whitespace_trailing_foreground "#ff2020") ;; used in modes-setup/base-modes-setup.el
+(defconst whitespace_trailing_background "#070707") ;; used in modes-setup/base-modes-setup.el
+(defconst whitespace_trailing_weight 'light) ;; used in modes-setup/base-modes-setup.el
 
-(defconst hl_line_background "#101010") ;; used in base.el
-(defconst hl_region_background "#232323") ;; used in base.el
+(defconst hl_line_background "#101010") ;; used in modes-setup/base-modes-setup.el
+(defconst hl_region_background "#232323") ;; used in modes-setup/base-modes-setup.el
 
-(defconst cursor_color "green2") ;; used in base.el
+(defconst cursor_color "green2") ;; used in modes-setup/base-modes-setup.el
 
 ;; is need use undo-tree history files
-(defconst use_undo_tree_history_files t) ;; used in base.el
+(defconst use_undo_tree_history_files t) ;; used in modes-setup/base-modes-setup.el
 
 ;; define theme for window
 (set 'win-theme 'sunburst)
