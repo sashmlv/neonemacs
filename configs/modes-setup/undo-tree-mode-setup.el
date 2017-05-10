4c3064bb5de98af0d081fcede8e8bcdb88cf144e
sashlex <sashlex@yandex.ru>
2017-05-10 12:02:18 +0300

    Fix undo-tree library loading.

--- configs/modes-setup/undo-tree-mode-setup.el
+++ configs/modes-setup/undo-tree-mode-setup.el
@@ -4,27 +4,31 @@
 
 ;; * START undo-tree melpa plugin ( undo-redo ) setup *
 (require 'undo-tree)
-
 (global-undo-tree-mode t)
 
-;; START custom code
 ;; set hook that load undo-tree history files, when switching buffers
 (if use_undo_tree_history_files ;; "use_undo_tree_history_files" in index.el
     (progn
-      (remove-hook 'find-file-hook 'undo-tree-load-history-hook) ;; fix bug, with unwanted hook (when load history on emacs starting, which is worng)
+      (remove-hook 'find-file-hook 'undo-tree-load-history-hook) ;; disable default
       (setq undo-tree-auto-save-history t) ;; auto save history
       (setq undo-tree-history-directory-alist `(("." . ,undo_redo_dir))) ;; set history directory
-
       (defadvice undo-tree-make-history-save-file-name ;; enable compress history
           (after undo-tree activate)
         (setq ad-return-value (concat ad-return-value ".gz")))
-      (setq is_allow_execute_load_history_file t) ;; label for allowing execute (allow execute first on emacs initialization -> "t")
 
-      (defun set_allow_execute_load_history_file (orig-fun &rest args)
-        (setq is_allow_execute_load_history_file t) ;; set allow execute hook parameter
-        (apply orig-fun args) ;; call original function
+      (setq loaded_undo_tree_history_files nil) ;; list of loaded history files
+      (setq last_buffer_from_previous_session (buffer-name (car (buffer-list)))) ;; restore last used buffer, from previous session, from buffer list
+      (setq undo_tree_history_initialized nil) ;; is undo-tree initialized
+      (setq allow_execute_load_undo_tree_history_file t) ;; prevent execute while lazy loading (allow execute on first time when emacs starting, so set-> "t")
+
+      (defun set_allow_execute_load_undo_tree_history_file (orig-fun &rest args) ;; prevent execute while lazy load file
+        (setq allow_execute_load_undo_tree_history_file nil) ;; start prevent
+        (apply orig-fun args)
+        (setq allow_execute_load_undo_tree_history_file t) ;; stop prevent
         )
-      (advice-add 'walk-windows :around #'set_allow_execute_load_history_file) ;; hook launch on switch buffer manually
+      (advice-add 'desktop-lazy-create-buffer :around #'set_allow_execute_load_undo_tree_history_file)
+
+      (add-hook 'window-configuration-change-hook 'load_undo_tree_history_file ) ;; hook when current-buffer will be changed
 
       (defun load_undo_tree_history_file()
         (progn
@@ -34,10 +38,9 @@
                      (not (member (buffer-file-name (current-buffer)) loaded_undo_tree_history_files)) ;; if history file still not loaded and ... ( see below )
                      (eq (type-of (buffer-file-name (current-buffer))) 'string) ;; if file name string
                      (buffer-file-name (current-buffer)) ;; if current buffer has file, load this undo-tree history file.
-                     is_allow_execute_load_history_file ;; is allow execute
+                     allow_execute_load_undo_tree_history_file
                      )
                     (progn
-                      (setq is_allow_execute_load_history_file nil) ;; set back disallow execute hook
                       (undo-tree-load-history nil t) ;; load "current-buffer" history file, with disabled error message on file not found
                       (add-to-list 'loaded_undo_tree_history_files (buffer-file-name (current-buffer))) ;; save to list loaded history file, for prevent load again
                       )
@@ -47,14 +50,8 @@
             )
           )
         )
-
-      (setq loaded_undo_tree_history_files nil) ;; list of loaded history files
-      (setq last_buffer_from_previous_session (buffer-name (car (buffer-list)))) ;; restore last used buffer, from previous session, from buffer list
-      (setq undo_tree_history_initialized nil) ;; is undo-tree initialized
-      (add-hook 'window-configuration-change-hook 'load_undo_tree_history_file ) ;; hook when current-buffer will be changed
       )
   )
-
 ;; END
 ;; * END *
 
