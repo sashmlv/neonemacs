3dec1acc64fd0f7813c8798a099b6f88959d7158
sashmlv <sashmlv@yandex.ru>
2019-05-05 18:36:09 +0300

    Rollback dired-k hook.

--- configs/modes/base-setup.el
+++ configs/modes/base-setup.el
@@ -195,8 +195,7 @@
 ;; You can use dired-k alternative to revert-buffer
 (define-key dired-mode-map (kbd "g") 'dired-k)
 ;; always execute dired-k when dired buffer is opened
-;; (add-hook 'dired-initial-position-hook 'dired-k)
-(add-hook 'dired-mode-hook 'dired-k)
+(add-hook 'dired-initial-position-hook 'dired-k)
 ;; (add-hook 'dired-after-readin-hook #'dired-k-no-revert)
 
 ;; * Disable error on free variables *
