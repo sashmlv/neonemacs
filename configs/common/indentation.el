eebb34c7976d9c28808247c433085c6741a0fcf9
sashmlv <sashmlv@yandex.ru>
2019-07-13 20:24:09 +0300

    Fix tab size.

--- configs/common/indentation.el
+++ configs/common/indentation.el
@@ -5,14 +5,14 @@
 ;; highlight indentation hook
 (highlight-indentation-mode 1) ;; disable error
 
-(setq indent-tabs-mode nil)
-(setq tab-width current_indent)
-(setq c-basic-offset current_indent)
-(setq js-indent-level current_indent)
-(setq css-indent-offset current_indent)
-(setq typescript-indent-level current_indent)
-(setq rust-indent-offset current_indent)
-(setq yaml-indent-offset 2)
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width current_indent)
+(setq-default c-basic-offset current_indent)
+(setq-default js-indent-level current_indent)
+(setq-default css-indent-offset current_indent)
+(setq-default typescript-indent-level current_indent)
+(setq-default rust-indent-offset current_indent)
+(setq-default yaml-indent-offset 2)
 
 (add-hook 'highlight-indentation-mode-hook
           (lambda()
@@ -28,7 +28,7 @@
                      indentation_current_line_color)
 
 ;; html-mode indentation https://www.emacswiki.org/emacs/IndentingHtml
-(setq sgml-basic-offset current_indent)
+(setq-default sgml-basic-offset current_indent)
 (add-hook 'html-mode-hook
           (lambda () (set (make-local-variable 'sgml-basic-offset) current_indent)))
 
