c1801a3ff865357ba86551cd01efabb55827ffa4
sashmlv <sashmlv@yandex.ru>
2019-08-25 23:32:10 +0300

    Fix commit typo.

---
+++ configs/modes/projectile.el
@@ -0,0 +1,10 @@
+;;; projectile.el --- Projectile configuration
+;;; Commentary:
+;;; Code:
+
+;; project management
+(projectile-mode)
+(setq projectile-switch-project-action 'projectile-dired)
+(setq projectile-completion-system 'ivy)
+
+;;; projectile.el ends here
