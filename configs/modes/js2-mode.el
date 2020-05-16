9170236c9c5cbcbf06ff406fd2d3b831230d5b0c
sashmlv <sashmlv@yandex.ru>
2020-05-16 15:10:23 +0300

    Fix js indentation for variables declaration.

--- configs/modes/js2-mode.el
+++ configs/modes/js2-mode.el
@@ -6,7 +6,7 @@
 (custom-set-faces '(js2-error ((t (:foreground "coral")))))
 
 ;; turn off all warnings in js2-mode
-(setq js2-mode-show-parse-errors t
+(setq-default js2-mode-show-parse-errors t
       js2-mode-show-strict-warnings nil
       js2-strict-missing-semi-warning nil
       js2-strict-trailing-comma-warning nil)
@@ -15,37 +15,70 @@
 ;; {
 ;;    x: 123
 ;; }
-(setq js-curly-indent-offset 0)
+(setq-default js-curly-indent-offset 0)
 
 ;; Example:
 ;; func()
 ;;   .then()
 ;;   .catch()
-(setq js-expr-indent-offset 0)
-;; (setq js-expr-indent-offset (- js-indent-level))
+(setq-default js-expr-indent-offset 0)
+;; (setq-default js-expr-indent-offset (- js-indent-level))
 
 ;; Example:
 ;; x.map( very =>
 ;;        very
 ;;      )
-(setq js-indent-align-list-continuation t)
+(setq-default js-indent-align-list-continuation t)
 
 ;; Example:
 ;; if(
 ;;      true
 ;;   )
-(setq js-paren-indent-offset 0)
+(setq-default js-paren-indent-offset 0)
 
 ;; Example:
 ;; var x = [
 ;;    true
 ;; ];
-(setq js-square-indent-offset 0)
+(setq-default js-square-indent-offset 0)
 
 ;; Example:
 ;; switch (true) {
 ;; case variable:
-(setq js-switch-indent-offset 10)
+(setq-default js-switch-indent-offset 0)
+
+;; Example:
+;;   var o = {
+;;       foo: 3
+;;   },
+;;       bar = 2;
+;; (setq-default js-indent-first-init 'dynamic) ;; 'dynamic, nil, t
+(setq-default js-indent-first-init nil)
+
+;; Example:
+;; let x = svg.mumble()
+;;            .chained;
+(setq-default js-chain-indent 0)
+
+;; Example:
+;; function x(arg1,
+;;            arg2,
+;;            arg3)
+(setq-default js-indent-align-list-continuation nil)
+
+;; Fix indentation for js variables declaration
+;; (fix indentation from original function, wich handle indent for this)
+(defun fix_vars_indent (orig-fun &rest args)
+  (setq orig_value (apply orig-fun args))
+  (if (and orig_value (>= orig_value js-indent-level))
+
+      ;; one level back indentation
+      (* (- (/ orig_value js-indent-level) 1) js-indent-level)
+    nil))
+
+(advice-add #'js--multi-line-declaration-indentation :around #'fix_vars_indent)
+;; for disable fix indentation run this:
+;; (advice-remove #'js--multi-line-declaration-indentation #'fix_vars_indent)
 
 ;; * Disable error on free variables *
 ;; Local Variables:
