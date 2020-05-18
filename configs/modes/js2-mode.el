bfec1e580c2a800ff0cbf35c28a595cc0fdc025d
sashmlv <sashmlv@yandex.ru>
2020-05-18 22:18:41 +0300

    Rework fix for variable names indentation.

--- configs/modes/js2-mode.el
+++ configs/modes/js2-mode.el
@@ -25,8 +25,8 @@
 ;; (setq-default js-expr-indent-offset (- js-indent-level))
 
 ;; Example:
-;; x.map( very =>
-;;        very
+;; x.map( val =>
+;;        val
 ;;      )
 (setq-default js-indent-align-list-continuation t)
 
@@ -52,7 +52,7 @@
 ;;       foo: 3
 ;;   },
 ;;       bar = 2;
-(setq-default js-indent-first-init nil) ;; 'dynamic, nil, t
+(setq-default js-indent-first-init 'dynamic) ;; 'dynamic, nil, t
 
 ;; Example:
 ;; let x = svg.mumble()
@@ -65,18 +65,87 @@
 ;;            arg3)
 (setq-default js-indent-align-list-continuation nil)
 
-;; Fix indentation for multiline variable declaration
-;; see emacs source - js.el, at end of function: js--multi-line-declaration-indentation
-;; ( when returns value, rollback goto-char and return current-column with indentation )
-(when (eq js-indent-first-init nil)
-  (defun fix_vars_indent (orig-fun &rest args)
+;; Fix variables indentation
+;; From:
+;;   let a = 1,
+;;       b = 2,
+;;       c = 3;
+;; To:
+;;   let a = 1,
+;;     b = 2,
+;;     c = 3;
+(defconst fix_variables_indentation t)
+(when fix_variables_indentation
+
+  ;; Fix indentation for multiline variables names declarations
+  ;; see emacs source - js.el, at end of function: js--multi-line-declaration-indentation
+  ;; ( when returns value, rollback goto-char and return current-column with indentation )
+  ;; Example:
+  ;; let name_a = 1,
+  ;;   name_b = 2,
+  ;;   name_c = 3;
+  (defun fix_var_name_indent (orig-fun &rest args)
     (when (apply orig-fun args)
       (goto-char (match-beginning 0))
       (+ (current-column) js-indent-level)))
-  (advice-add #'js--multi-line-declaration-indentation :around #'fix_vars_indent))
 
-;; for disable fix run this:
-;; (advice-remove #'js--multi-line-declaration-indentation #'fix_vars_indent)
+  ;; init fix
+  (advice-add #'js--multi-line-declaration-indentation :around #'fix_var_name_indent)
+  ;; (advice-remove #'js--maybe-goto-declaration-keyword-end #'fix_var_name_indent)
+
+  ;; Fix indentation at begining variable declaration
+  ;; see emacs source - js.el, at part where 'dynamic and t conditions: js--maybe-goto-declaration-keyword-end
+  ;; ( for 'dynamic and t value: replace function result into current-column with indentation )
+  ;; Example:
+  ;;     let a = {
+  ;; -->     x: 1,
+  ;; -->   },
+  ;;       b = {
+  ;;         y: 2,
+  ;;       };
+  (defun fix_var_indent_begining (orig-fun &rest args)
+    (cond
+
+     ;; 'dynamic
+     ((eq js-indent-first-init 'dynamic)
+      (apply 'dynamic_part_replacer_of_js--maybe-goto-declaration-keyword-end args))
+
+     ;; t
+     ((eq js-indent-first-init t)
+      (when (looking-at js--declaration-keyword-re)
+        (goto-char (1+ (+ (current-column) js-indent-level)))))
+
+     ;; otherwise
+     (t (apply orig-fun args))))
+
+  ;; init fix
+  (when (or (eq js-indent-first-init 'dynamic) (eq js-indent-first-init t))
+    (advice-add #'js--maybe-goto-declaration-keyword-end :around #'fix_var_indent_begining))
+  ;; (advice-remove #'js--maybe-goto-declaration-keyword-end #'fix_var_indent_begining)
+
+  (defun dynamic_part_replacer_of_js--maybe-goto-declaration-keyword-end (parse-status)
+    (let ((bracket (nth 1 parse-status))
+          declaration-keyword-end
+          at-closing-bracket-p
+          forward-sexp-function ; Use Lisp version.
+          comma-p)
+      (when (looking-at js--declaration-keyword-re)
+        (setq declaration-keyword-end (match-end 0))
+        (save-excursion
+          (goto-char bracket)
+          (setq at-closing-bracket-p
+                (condition-case nil
+                    (progn
+                      (forward-sexp)
+                      t)
+                  (error nil)))
+          (when at-closing-bracket-p
+            (while (forward-comment 1))
+            (setq comma-p (looking-at-p ","))))
+        (when comma-p
+          (goto-char (1+ (+ (current-column) js-indent-level)))
+          ))))
+  )
 
 ;; * Disable error on free variables *
 ;; Local Variables:
