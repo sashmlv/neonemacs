4cdc44f266c36dca87d3c58777857d9e16898570
sashmlv <sashmlv@yandex.ru>
2020-06-08 23:41:56 +0300

    Fix.

--- configs/common/hooks.el
+++ configs/common/hooks.el
@@ -12,7 +12,7 @@
             (smartscan-mode t)
             (highlight-indentation-mode t) ;; (highlight-indent-guides-mode 1)
             (highlight-symbol-mode t) ;; highlight occurrences
-            (highlight-100-mode t) ;; highlight 80 column rule
+            (highlight-100-mode t) ;; highlight 100 column rule
             (rainbow-delimiters-mode t) ;; rainbow parentheses
             ))
 
