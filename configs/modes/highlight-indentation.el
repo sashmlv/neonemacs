de6ece75f05b9ec7d2be9f7df2d477583902bd87
sashmlv <sashmlv@yandex.ru>
2020-07-03 17:59:40 +0300

    Add highlight indentation from editorconfig.

--- configs/modes/highlight-indentation.el
+++ configs/modes/highlight-indentation.el
@@ -5,15 +5,22 @@
 ;; highlight indentation hook
 (highlight-indentation-mode t)
 
+;; set current_indent value from .editorconfig file
+(defun set_current_indent_from_editorconfig (props)
+   (progn
+      (if (gethash 'indent_size props)
+         (setq current_indent (string-to-number (gethash 'indent_size props))))))
+(remove-hook 'editorconfig-after-apply-functions 'set_current_indent_from_editorconfig)
+(add-hook 'editorconfig-after-apply-functions 'set_current_indent_from_editorconfig)
+
 (defun highlight_indentation_offset ()
-  (progn
-    (setq highlight-indentation-offset current_indent) ;; set indent size
-    (cond ((eq major-mode 'yaml-mode)
-          (setq highlight-indentation-offset yaml-indent-offset)))
-   ))
+   (progn
+      (setq highlight-indentation-offset current_indent) ;; set indent size
+      (cond ((eq major-mode 'yaml-mode)
+               (setq highlight-indentation-offset yaml-indent-offset)))
+      ))
 (remove-hook 'highlight-indentation-mode-hook 'highlight_indentation_offset)
 (add-hook 'highlight-indentation-mode-hook 'highlight_indentation_offset)
-
 ;; * Disable error on free variables *
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars)
