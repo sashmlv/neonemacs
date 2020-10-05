27406cee29f969927df446f2834d246c52dabb68
sashmlv <sashmlv@yandex.ru>
2020-10-05 22:09:52 +0300

    Add lang autodetect for google translate.

--- config/lisp/google-translate.el
+++ config/lisp/google-translate.el
@@ -6,5 +6,16 @@
 (require 'google-translate-default-ui)
 (setq google-translate-default-source-language "en")
 (setq google-translate-default-target-language "ru")
+(global-set-key (kbd "C-c t") 'google_translate)
+
+(defun google_translate ()
+  (interactive)
+  (cond ((or(looking-at "[a-zA-Z]")
+            (string-match "[a-zA-Z]" (string (char-before))))
+         (google-translate-at-point))
+        ((or(looking-at "[а-яА-Я]")
+            (string-match "[а-яА-Я]" (string (char-before))))
+         (google-translate-at-point-reverse))
+        (t (google-translate-at-point))))
 
 ;;; google-translate.el ends here
