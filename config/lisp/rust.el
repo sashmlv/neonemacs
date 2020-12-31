9a156c471df2938e83159cd24e95dd00c13aeb9a
sashmlv <sashmlv@yandex.ru>
2020-12-31 18:35:27 +0300

    Fix racer.

--- config/lisp/rust.el
+++ config/lisp/rust.el
@@ -10,4 +10,13 @@
 (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
 (setq company-tooltip-align-annotations t)
 
+;; tmp fix, remove me
+(setq racer-rust-src-path
+      (let* ((sysroot (string-trim
+                       (shell-command-to-string "rustc --print sysroot")))
+             (lib-path (concat sysroot "/lib/rustlib/src/rust/library"))
+              (src-path (concat sysroot "/lib/rustlib/src/rust/src")))
+        (or (when (file-exists-p lib-path) lib-path)
+            (when (file-exists-p src-path) src-path))))
+
 ;;; rust.el ends here
--- doc/racer.md
+++ doc/racer.md
@@ -4,3 +4,4 @@
 * ```rustup toolchain add nightly```
 * ```rustup component add rust-src```
 * ```cargo +nightly install racer```
+* install [emacs-racer](https://github.com/racer-rust/emacs-racer)
