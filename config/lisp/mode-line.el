b209d58448204eadbe4f6fd6f23ddcf73dbe131a
sashmlv <sashmlv@yandex.ru>
2021-08-15 23:48:53 +0300

    Update colors.

--- config/lisp/mode-line.el
+++ config/lisp/mode-line.el
@@ -11,30 +11,9 @@
 (setq auto-revert-check-vc-info t)
 (global-total-lines-mode) ;; `total-lines' variable in modeline
 
-(defface buffer_path
+(defface mode_line_white
   '((t :foreground "#FFFFFF" :weight normal))
   "Face for highlight current buffer path.")
-(defface buffer_path_dir
-  '((t :foreground "#B8F2FE" :weight normal))
-  "Face for highlight directory of current buffer.")
-(defface buffer_path_file
-  '((t :foreground "#EDC9AF" :weight normal))
-  "Face for highlight file of current buffer.")
-(defface mode_line_position
-  '((t :foreground "#FFFFFF" :weight normal))
-  "Face for highlight cursor position.")
-(defface mode_line_buffer_size
-  '((t :foreground "#FFFFFF" :weight normal))
-  "Face for highlight buffer size.")
-(defface mode_line_modified
-  '((t :foreground "#FFFFFF" :weight normal))
-  "Face for highlight buffer modified.")
-(defface mode_line_mode_name
-  '((t :foreground "#FFFFFF" :weight normal))
-  "Face for highlight mode name.")
-(defface mode_line_minor_mode_alist
-  '((t :foreground "#FFFFFF" :weight normal))
-  "Face for highlight minor mode alist.")
 
 (defun buffer_path(&optional path_limit part_length)
   "Get active buffer path, with limit path length"
@@ -44,44 +23,29 @@
     (setq curr_path (or buffer-file-name (directory-file-name (expand-file-name default-directory))))
     (or path_limit (setq path_limit default_path_limit))
     (cond ((not part_length)
-           (setq path (format
-                       "%s%s%s"
-                       (propertize
-                        (directory-file-name (file-name-directory (directory-file-name (file-name-directory curr_path))))
-                        'face 'buffer_path) ;; grand parent
-                       (propertize
-                        (concat "/" (file-name-nondirectory (directory-file-name (file-name-directory curr_path))))
-                        'face 'buffer_path_dir) ;; parent
-                       (propertize
-                        (concat "/" (file-name-nondirectory curr_path))
-                        'face 'buffer_path_file)))) ;; file, dir
+           (setq path (list
+                        (directory-file-name (file-name-directory (directory-file-name (file-name-directory curr_path)))) ;; grand parent
+                        (concat "/" (file-name-nondirectory (directory-file-name (file-name-directory curr_path)))) ;; parent
+                        (concat "/" (file-name-nondirectory curr_path))))) ;; file, dir
           (part_length
-           (setq path (format
-                       "%s%s%s"
-                       (propertize (mapconcat
-                         'identity
-                         (let ((result ()))
-                           (dolist (element (split-string curr_path "\\/"))
-                             (if (< part_length (length element))
-                                 (push (substring element 0 part_length) result)
-                               (push element result)))
-                           (nreverse (nthcdr 2 result)))
-                         "/") 'face 'buffer_path)
-                       (propertize (concat
-                                    "/"
-                                    (file-name-nondirectory (directory-file-name (file-name-directory curr_path))))
-                                   'face 'buffer_path_dir) ;; file or dir parent name
-                       (propertize (concat
-                                    "/"
-                                    (file-name-nondirectory curr_path))
-                                   'face 'buffer_path_file))))) ;; file or dir name
-    (if (> (length path) path_limit)
+           (setq path (list
+                              (mapconcat
+                               'identity
+                               (let ((result ()))
+                                 (dolist (element (split-string curr_path "\\/"))
+                                   (if (< part_length (length element))
+                                       (push (substring element 0 part_length) result)
+                                     (push element result)))
+                                 (nreverse (nthcdr 2 result)))
+                               "/")
+                              (concat "/" (file-name-nondirectory (directory-file-name (file-name-directory curr_path)))) ;; file or dir parent name
+                              (concat "/" (file-name-nondirectory curr_path)))))) ;; file or dir name
+    (if (> (length (mapconcat 'identity path "")) path_limit)
         (progn
           (if (not part_length) (setq part_length (1+ default_part_length)))
           (setq part_length (1- part_length))
           (if (>= part_length 0)
-              (setq path (buffer_path path_limit part_length)))
-            (replace-regexp-in-string "\\/\\{2,\\}" "" path))
+              (setq path (buffer_path path_limit part_length))))
       path)))
 
 (defun mode-line-render (start_part end_part)
@@ -89,26 +53,32 @@
   (let* ((start_length (length start_part))
          (end_length (length end_part))
          (free_space_length (- (window-width) start_length end_length))
-         (buffer_path_string (buffer_path free_space_length)))
-    (format "%s%s%s" start_part buffer_path_string end_part)))
+         (buffer_path_list (buffer_path free_space_length)))
+         (format "%s%s%s"
+                 start_part
+                 (format-mode-line (list
+                                    '(:eval (propertize (format "%s" (nth 0 buffer_path_list)) 'face '(:foreground "#00BBB3" :weight normal)))
+                                    '(:eval (propertize (format "%s" (nth 1 buffer_path_list)) 'face '(:foreground "#45EDE6" :weight normal)))
+                                    '(:eval (propertize (format "%s" (nth 2 buffer_path_list)) 'face '(:foreground "#60FDF4" :weight normal)))))
+                 end_part)))
 
 (setq mode-line-format
       '((:eval (mode-line-render
                 (format-mode-line (list
                                    "%e"
                                    'mode-line-front-space
-                                   '(:eval (propertize (format-time-string "%H:%M ") 'face 'mode_line_position))
-                                   '(:eval (propertize (format "%%l:%d:%%C " total-lines) 'face 'mode_line_position)) ;; line : total lines : column
-                                   '(:eval (propertize (replace-regexp-in-string "(\\|)" "" (format "%s " mode-line-remote)) 'face 'mode_line_position))
+                                   '(:eval (propertize (format-time-string "%H:%M ") 'face 'mode_line_white))
+                                   '(:eval (propertize (format "%%l:%d:%%C " total-lines 'face 'mode_line_white))) ;; line : total lines : column
+                                   '(:eval (propertize (replace-regexp-in-string "(\\|)" "" (format "%s " mode-line-remote)) 'face 'mode_line_white))
                                    ))
                 (format-mode-line (list
                                    " "
-                                   '(:eval (propertize "%+" 'face 'mode_line_position)) ;; readonly, modified
+                                   '(:eval (propertize "%+" 'face 'mode_line_white)) ;; readonly, modified
                                    '(:eval (propertize (format " %s " (vc-state (buffer-file-name (current-buffer))))))
-                                   '(:eval (propertize (car (vc-git-branches))) 'face 'mode_line_position)
-                                   '(:eval (propertize " %I " 'face 'mode_line_buffer_size)) ;; file size
-                                   '(:eval (propertize (format "%s " buffer-file-coding-system) 'face 'mode_line_position))
-                                   '(:propertize mode-name face mode_line_mode_name)
-                                   '(:propertize minor-mode-alist face mode_line_minor_mode_alist)))))))
+                                   '(:eval (propertize (car (vc-git-branches))) 'face 'mode_line_white)
+                                   '(:eval (propertize " %I " 'face 'mode_line_white)) ;; file size
+                                   '(:eval (propertize (format "%s " buffer-file-coding-system) 'face 'mode_line_white))
+                                   '(:propertize mode-name face mode_line_white)
+                                   '(:propertize minor-mode-alist face mode_line_white)))))))
 
 ;;; mode-line.el ends here
