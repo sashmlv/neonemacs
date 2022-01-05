ddf3eecd844680db9d30c135106b9c934923624b
sashmlv <sashmlv@yandex.ru>
2022-01-05 20:58:48 +0300

    Fix company.

--- config/lisp/company.el
+++ config/lisp/company.el
@@ -1,9 +1,18 @@
-;;; company.el --- company configuration
+;;; company.el --- company (autocompletition) configuration
 ;;; Commentary:
 ;;; Code:
 
-;; autocompletition
 (add-hook 'after-init-hook 'global-company-mode)
-(company-statistics-mode)
+
+(setq company-idle-delay nil)
+(setq company-dabbrev-downcase nil)
+(setq company-dabbrev-code-ignore-case t)
+
+(defun yas-expand-or-company-complete (&optional arg)
+  "yas or complete"
+  (interactive)
+  (or (yas-expand) (company-indent-or-complete-common arg)))
+
+(define-key prog-mode-map (kbd "<tab>") #'yas-expand-or-company-complete)
 
 ;;; company.el ends here
