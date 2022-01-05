c7aed4b210ee2ac357ffc31add169350c25f6550
sashmlv <sashmlv@yandex.ru>
2022-01-05 22:59:44 +0300

    Fix backgrounds.

--- config/lisp/highlight-words.el
+++ config/lisp/highlight-words.el
@@ -6,16 +6,16 @@
   ;; "Highlight words in code"
   (font-lock-add-keywords
    nil
-   '(("\\(TODO:?\\)"   1 '(:foreground "#FF5500" :background "#151515") prepend)
-     ("\\(NEXT:?\\)"   1 '(:foreground "#FF5500" :background "#151515") prepend)
-     ("\\(DONT:?\\)"   1 '(:foreground "#FF5500" :background "#151515") prepend)
-     ("\\(FIXME:?\\)"  1 '(:foreground "#FF5500" :background "#151515") prepend)
-     ("\\(FAIL:?\\)"   1 '(:foreground "#FF5500" :background "#151515") prepend)
-     ("\\(KEEP:?\\)"   1 '(:foreground "#FF5500" :background "#151515") prepend)
-     ("\\(NOTE:?\\)"   1 '(:foreground "#00FF55" :background "#151515") prepend)
-     ("\\(INFO:?\\)"   1 '(:foreground "#00FF55" :background "#151515") prepend)
-     ("\\(DONE:?\\)"   1 '(:foreground "#00FF55" :background "#151515") prepend)
-     ("\\(TEMP:?\\)"   1 '(:foreground "#0055FF" :background "#151515") prepend)
-     ("\\(HACK:?\\)"   1 '(:foreground "#0055FF" :background "#151515") prepend))))
+   '(("\\(TODO:?\\)"   1 '(:foreground "#FF5500") prepend)
+     ("\\(NEXT:?\\)"   1 '(:foreground "#FF5500") prepend)
+     ("\\(DONT:?\\)"   1 '(:foreground "#FF5500") prepend)
+     ("\\(FIXME:?\\)"  1 '(:foreground "#FF5500") prepend)
+     ("\\(FAIL:?\\)"   1 '(:foreground "#FF5500") prepend)
+     ("\\(KEEP:?\\)"   1 '(:foreground "#FF5500") prepend)
+     ("\\(NOTE:?\\)"   1 '(:foreground "#00FF55") prepend)
+     ("\\(INFO:?\\)"   1 '(:foreground "#00FF55") prepend)
+     ("\\(DONE:?\\)"   1 '(:foreground "#00FF55") prepend)
+     ("\\(TEMP:?\\)"   1 '(:foreground "#0055FF") prepend)
+     ("\\(HACK:?\\)"   1 '(:foreground "#0055FF") prepend))))
 
 ;;; highlight-words.el ends here
