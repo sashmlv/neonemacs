1b24d016e6860b524536133c66465ee33b67ee3a
sashmlv <sashmlv@yandex.ru>
2022-01-15 21:23:42 +0300

    Fix company.

--- config/lisp/company.el
+++ config/lisp/company.el
@@ -4,28 +4,27 @@
 
 (add-hook 'after-init-hook 'global-company-mode)
 
-(define-key prog-mode-map (kbd "<M-tab>") #'company-complete)
-
 (setq company-idle-delay nil)
 (setq company-dabbrev-downcase nil)
-(setq company-minimum-prefix-length 1)
 (setq company-dabbrev-code-ignore-case t)
 
 (defun fix-prog-mode-company (&optional arg)
   (interactive)
   (or (yas-expand)
-      (if (>= (length (company-grab-symbol)) company-minimum-prefix-length)
+      (if (string-match-p "[^[:space:]\n]" (string (preceding-char)))
           (company-indent-or-complete-common arg)
         (indent-for-tab-command))))
-(define-key prog-mode-map (kbd "<tab>") #'fix-prog-mode-company)
 
 (defun fix-restclient-mode-company (&optional arg)
   (interactive)
   (or (yas-expand)
-      (if (>= (length (company-grab-symbol)) company-minimum-prefix-length)
+      (if (string-match-p "[^[:space:]\n]" (string (preceding-char)))
           (company-indent-or-complete-common arg)
         (restclient-toggle-body-visibility-or-indent))))
 (with-eval-after-load 'company (add-to-list 'company-backends 'company-restclient))
+
+(define-key prog-mode-map (kbd "<M-tab>") #'company-complete)
+(define-key prog-mode-map (kbd "<tab>") #'fix-prog-mode-company)
 (with-eval-after-load 'restclient (define-key restclient-mode-map (kbd "<tab>") #'fix-restclient-mode-company))
 
 ;;; company.el ends here
